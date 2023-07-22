ccea0358807830dcd4f00f43460d44c89b116f96
giovannicarpentieri <carpentierigiovanni@gmail.com>
2023-07-22 21:26:32 +0200

    Test

---
+++ logics.js
@@ -0,0 +1,19 @@
+
+
+
+
+// Aggiungi questo all'inizio del tuo file JavaScript
+if (localStorage.getItem("theme") === "dark") {
+  document.body.classList.add("dark-mode");
+}
+
+// Modifica l'handler del click sul tuo pulsante di commutazione
+toggleButton.addEventListener("click", function() {
+  document.body.classList.toggle("dark-mode");
+
+  let theme = "light";
+  if (document.body.classList.contains("dark-mode")) {
+      theme = "dark";
+  }
+  localStorage.setItem("theme", theme);
+});
