a1181ca90ff29287518cb4a9182748dc1d2298da
giovannicarpentieri <carpentierigiovanni@gmail.com>
2023-07-28 22:53:01 +0200

    Style

--- logics.js
+++ logics.js
@@ -1,29 +1,29 @@
-// Seleziona gli elementi
-var submitButton = document.querySelector("#submit");
-var loginDiv = document.querySelector("#login");
-var contentDiv = document.querySelector("#content");
-var errorMessage = document.querySelector("#error-message");
-var maintenanceDiv = document.querySelector("#maintenance");
+// // Seleziona gli elementi
+// var submitButton = document.querySelector("#submit");
+// var loginDiv = document.querySelector("#login");
+// var contentDiv = document.querySelector("#content");
+// var errorMessage = document.querySelector("#error-message");
+// var maintenanceDiv = document.querySelector("#maintenance");
 
-// Verifica le credenziali quando il pulsante viene premuto
-submitButton.addEventListener("click", function() {
-  var username = document.querySelector("#username").value;
-  var password = document.querySelector("#password").value;
+// // Verifica le credenziali quando il pulsante viene premuto
+// submitButton.addEventListener("click", function() {
+//   var username = document.querySelector("#username").value;
+//   var password = document.querySelector("#password").value;
 
-  if (username === "gianni" && password === "carp") {
-    localStorage.setItem("isLoggedIn", true);
-    loginDiv.style.display = "none";
-    maintenanceDiv.style.display = "none";
-    contentDiv.style.display = "block";
-  } else {
-    errorMessage.style.display = "block";
-    errorMessage.textContent = "Username o password errati!";
-  }
-});
+//   if (username === "gianni" && password === "carp") {
+//     localStorage.setItem("isLoggedIn", true);
+//     loginDiv.style.display = "none";
+//     maintenanceDiv.style.display = "none";
+//     contentDiv.style.display = "block";
+//   } else {
+//     errorMessage.style.display = "block";
+//     errorMessage.textContent = "Username o password errati!";
+//   }
+// });
 
-// Se l'utente è già loggato, nasconde il modulo di login e il messaggio di manutenzione
-if (localStorage.getItem("isLoggedIn")) {
-  loginDiv.style.display = "none";
-  maintenanceDiv.style.display = "none";
-  contentDiv.style.display = "flex";
-}
+// // Se l'utente è già loggato, nasconde il modulo di login e il messaggio di manutenzione
+// if (localStorage.getItem("isLoggedIn")) {
+//   loginDiv.style.display = "none";
+//   maintenanceDiv.style.display = "none";
+//   contentDiv.style.display = "flex";
+// }
