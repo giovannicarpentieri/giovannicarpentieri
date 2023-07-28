078c18d9d8e2634499c028d9054e5bfdbb29ead1
giovannicarpentieri <carpentierigiovanni@gmail.com>
2023-07-28 21:22:08 +0200

    Style

--- logics.js
+++ logics.js
@@ -1,39 +1,9 @@
 // Seleziona gli elementi
-var toggleButton = document.querySelector("#theme-toggle-button");
 var submitButton = document.querySelector("#submit");
 var loginDiv = document.querySelector("#login");
 var contentDiv = document.querySelector("#content");
 var errorMessage = document.querySelector("#error-message");
-var lightIcon = document.querySelector("#light-icon");
-var darkIcon = document.querySelector("#dark-icon");
-
-// Funzione per cambiare il tema
-function changeTheme() {
-  document.body.classList.toggle("dark-mode");
-
-  let theme = "light";
-  if (document.body.classList.contains("dark-mode")) {
-      theme = "dark";
-  }
-  localStorage.setItem("theme", theme);
-
-  // Alterna le icone
-  if (theme === "dark") {
-    lightIcon.style.display = "none";
-    darkIcon.style.display = "block";
-  } else {
-    lightIcon.style.display = "block";
-    darkIcon.style.display = "none";
-  }
-}
-
-// Imposta il tema iniziale
-if (localStorage.getItem("theme") === "dark") {
-  changeTheme();
-}
-
-// Cambia il tema quando il pulsante viene premuto
-toggleButton.addEventListener("click", changeTheme);
+var maintenanceDiv = document.querySelector("#maintenance");
 
 // Verifica le credenziali quando il pulsante viene premuto
 submitButton.addEventListener("click", function() {
@@ -41,11 +11,19 @@ submitButton.addEventListener("click", function() {
   var password = document.querySelector("#password").value;
 
   if (username === "gianni" && password === "carp") {
+    localStorage.setItem("isLoggedIn", true);
     loginDiv.style.display = "none";
-    maintenance.style.display = "none";
+    maintenanceDiv.style.display = "none";
     contentDiv.style.display = "block";
   } else {
     errorMessage.style.display = "block";
     errorMessage.textContent = "Username o password errati!";
   }
 });
+
+// Se l'utente è già loggato, nasconde il modulo di login e il messaggio di manutenzione
+if (localStorage.getItem("isLoggedIn")) {
+  loginDiv.style.display = "none";
+  maintenanceDiv.style.display = "none";
+  contentDiv.style.display = "flex";
+}
